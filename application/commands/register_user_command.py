777d10eb6671e714d13a4e89e4359cf9ac7c576a
mundstock <70247406+JorkDev@users.noreply.github.com>
2024-09-16 23:29:28 -0500

    Refactored to delegate logic to RegisterUser use case

--- application/commands/register_user_command.py
+++ application/commands/register_user_command.py
@@ -1,17 +1,9 @@
-from infrastructure.repositories.user_repository import UserRepository
-from core.entities.user import User
+from core.use_cases.register_user import RegisterUser
 
 class RegisterUserCommand:
-    def __init__(self, user_repository: UserRepository):
-        self.user_repository = user_repository
+    def __init__(self, user_repo):
+        self.user_repo = user_repo
 
     def handle(self, username: str, email: str):
-        existing_user = self.user_repository.find_by_username_or_email(username, email)
-        if existing_user:
-            raise ValueError("Usuario y/o correo existentes")
-        
-        user = User(username=username, email=email)
-
-        self.user_repository.save(user)
-
-        return user
+        use_case = RegisterUser(self.user_repo)
+        return use_case.execute(username, email)
