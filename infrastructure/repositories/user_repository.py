e39b282823f0606a0ba19acdc0645159c8e8bc0d
mundstock <70247406+JorkDev@users.noreply.github.com>
2024-09-16 23:26:27 -0500

    Refactored to include find by username or email and updated save method

--- infrastructure/repositories/user_repository.py
+++ infrastructure/repositories/user_repository.py
@@ -1,18 +1,20 @@
-from sqlalchemy.orm import Session
+from core.entities.user import User
 from infrastructure.database.models import UserModel
+from sqlalchemy.orm import Session
+from typing import Optional
+from uuid import UUID
 
 class UserRepository:
     def __init__(self, db: Session):
         self.db = db
 
-    def find_by_username_or_email(self, username: str, email: str):
-        return self.db.query(UserModel).filter(
-            (UserModel.username == username) | (UserModel.email == email)
-        ).first()
-
-    def save(self, user):
-        new_user = UserModel(username=user.username, email=user.email)
-        self.db.add(new_user)
+    def add(self, user: User):
+        user_model = UserModel(id=str(user.id), username=user.username, email=user.email)
+        self.db.add(user_model)
         self.db.commit()
-        self.db.refresh(new_user)
-        return new_user
+
+    def get_by_id(self, user_id: UUID) -> Optional[User]:
+        user_model = self.db.query(UserModel).filter_by(id=str(user_id)).first()
+        if user_model:
+            return User(id=user_model.id, username=user_model.username, email=user_model.email)
+        return None
