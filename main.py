4f0e31be3edb70747f4e5e478fc8ca5f02682d8a
mundstock <70247406+JorkDev@users.noreply.github.com>
2024-09-16 21:04:24 -0500

    Set up SQLAlchemy with MySQL and created basic UserModel

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://backend_user:password!@localhost/hexagonal_cqrs_backend"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class UserModel(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True)
+    email = Column(String(100), unique=True, index=True)
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
